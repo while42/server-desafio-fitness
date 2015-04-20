6db6ffce2499289a100ff2ee1130efa8da82b89d
Douglas C Campos <camargo@gmail.com>
2015-04-20 20:01:04 -0300

    Add route /academia/todos

---
+++ src/main/java/br/com/while42/treinofitness/controller/AcademiaController.java
@@ -0,0 +1,20 @@
+package br.com.while42.treinofitness.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import br.com.while42.treinofitness.model.Academia;
+import br.com.while42.treinofitness.repository.AcademiaRepository;
+
+@RestController
+@RequestMapping("/academia")
+public class AcademiaController {
+	
+	private @Autowired AcademiaRepository academiaRepository;
+
+	@RequestMapping(value = "/todos", method = RequestMethod.GET)
+	public Iterable<Academia> lista(){
+		return academiaRepository.findAll();
+	}
+}
