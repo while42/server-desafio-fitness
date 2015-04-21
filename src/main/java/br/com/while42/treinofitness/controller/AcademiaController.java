8d2126b2dd7a7610f4c9af9131bc916a0b35e139
David Robert <davidrobert@gmail.com>
2015-04-21 12:41:43 -0300

    Pequena correcao no parse da Academia

--- src/main/java/br/com/while42/treinofitness/controller/AcademiaController.java
+++ src/main/java/br/com/while42/treinofitness/controller/AcademiaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,7 +39,7 @@ public class AcademiaController {
 	}
 
 	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
-	public ResponseEntity<?> save(Academia academia) {
+	public ResponseEntity<?> save(@RequestBody Academia academia) {
 		if (academia == null) {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
