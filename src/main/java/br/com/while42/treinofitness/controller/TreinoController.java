5bb2e076e04710a4a1bf5bd3088c67beea84d82e
Douglas C Campos <camargo@gmail.com>
2015-04-20 22:56:48 -0300

    Add routes /treino/todos and /treino/<id>

--- src/main/java/br/com/while42/treinofitness/controller/TreinoController.java
+++ src/main/java/br/com/while42/treinofitness/controller/TreinoController.java
@@ -1,12 +1,31 @@
 package br.com.while42.treinofitness.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.while42.treinofitness.model.Treino;
+import br.com.while42.treinofitness.repository.TreinoRepository;
+
 @RestController
+@RequestMapping("/treino")
 public class TreinoController {
 
-	public TreinoController() {
+	private @Autowired TreinoRepository treinoRepository;
+	
+	@RequestMapping(value = "/todos", method = RequestMethod.GET)
+	public Iterable<Treino> lista() {
+		return treinoRepository.findAll();
 	}
 	
+	@RequestMapping(value = "/{treinoId}", method = RequestMethod.GET)
+	public Treino treino(@PathVariable String treinoId) {
+		return treinoRepository.findOne(Long.valueOf(treinoId));
+	}
+	
+	// TODO: Created method to return treinos by userId 
 	
 }
+
