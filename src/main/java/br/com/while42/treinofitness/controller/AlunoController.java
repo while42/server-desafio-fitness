fb0d2e93f4093a4beb1cf963120ce16c037bf435
Douglas C Campos <camargo@gmail.com>
2015-04-20 21:51:18 -0300

    Add route to get aluno by id

--- src/main/java/br/com/while42/treinofitness/controller/AlunoController.java
+++ src/main/java/br/com/while42/treinofitness/controller/AlunoController.java
@@ -1,11 +1,13 @@
 package br.com.while42.treinofitness.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.while42.treinofitness.model.Aluno;
+import br.com.while42.treinofitness.model.Instrutor;
 import br.com.while42.treinofitness.repository.AlunoRepository;
 
 @RestController
@@ -17,6 +19,11 @@ public class AlunoController {
 	@RequestMapping(value = "/todos", method = RequestMethod.GET)
 	public Iterable<Aluno> lista(){
 		return alunoRepository.findAll();
+	}
+	
+	@RequestMapping(value = "/{alunoId}", method = RequestMethod.GET)
+	public Aluno aluno(@PathVariable String alunoId) {
+		return alunoRepository.findOne(Long.valueOf(alunoId));
 	}
 
 }
