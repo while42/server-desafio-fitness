b51a339a408119117232fac3a48d7d8c492ccc8a
Douglas C Campos <camargo@gmail.com>
2015-04-21 12:37:45 -0300

    Added getStartedTime to show on route /status

--- src/main/java/br/com/while42/treinofitness/model/Status.java
+++ src/main/java/br/com/while42/treinofitness/model/Status.java
@@ -13,6 +13,10 @@ public class Status {
 		}
 	}
 	
+	public static String getTimestampStart() {
+		return timestampStartServer;
+	}
+	
 	public String getStartedTime() {
 		return timestampStartServer;
 	}
