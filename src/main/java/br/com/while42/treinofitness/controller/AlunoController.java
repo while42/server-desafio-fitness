234033ef0516701e2a5d3bd256d9e56f7c2ae60f
Douglas C Campos <camargo@gmail.com>
2015-04-20 21:23:44 -0300

    Add route /aluno/todos

--- src/main/java/br/com/while42/treinofitness/controller/AlunoController.java
+++ src/main/java/br/com/while42/treinofitness/controller/AlunoController.java
@@ -1,10 +1,22 @@
 package br.com.while42.treinofitness.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class AlunoController {
+import br.com.while42.treinofitness.model.Aluno;
+import br.com.while42.treinofitness.repository.AlunoRepository;
 
-	public AlunoController() {
+@RestController
+@RequestMapping("/aluno")
+public class AlunoController {
+	
+	private @Autowired AlunoRepository alunoRepository;
+
+	@RequestMapping(value = "/todos", method = RequestMethod.GET)
+	public Iterable<Aluno> lista(){
+		return alunoRepository.findAll();
 	}
+
 }
