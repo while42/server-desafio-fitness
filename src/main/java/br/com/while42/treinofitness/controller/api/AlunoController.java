1e0132fb6c89491e40b99f33b13608ca761ee291
David Robert <davidrobert@gmail.com>
2015-04-26 20:04:08 -0300

    Atualizacao AlunoController e AlunoWebCOntroller

--- src/main/java/br/com/while42/treinofitness/controller/api/AlunoController.java
+++ src/main/java/br/com/while42/treinofitness/controller/api/AlunoController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.while42.treinofitness.model.Academia;
 import br.com.while42.treinofitness.model.Aluno;
 import br.com.while42.treinofitness.repository.AlunoRepository;
 
--- src/main/java/br/com/while42/treinofitness/controller/web/AlunoWebController.java
+++ src/main/java/br/com/while42/treinofitness/controller/web/AlunoWebController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,6 +16,16 @@ import br.com.while42.treinofitness.repository.AlunoRepository;
 public class AlunoWebController {
 
 	private @Autowired AlunoRepository alunoRepository;
+	private @Autowired AlunoRepository academiaRepository;
+	
+	@RequestMapping(value = "/todos", method = RequestMethod.GET)
+	public String all(Model model){
+		Iterable<Aluno> academias = academiaRepository.findAll();
+		
+		model.addAttribute("alunos", academias);
+		
+		return "aluno-lista";
+	} 
 	
 	@RequestMapping(value = "/{alunoId}", method = RequestMethod.GET)
 	public String get(@PathVariable String alunoId, Model model) {
@@ -24,9 +35,28 @@ public class AlunoWebController {
 		if (aluno != null) {
 			model.addAttribute("id", aluno.getId());
 			model.addAttribute("username", aluno.getUsername());
+			return "aluno";
 		}
-		
-		return "aluno";
+
+		return "aluno-inexistente";
 	}
 
+	
+	@RequestMapping(value = "/{alunoId}", method = RequestMethod.DELETE)
+	public String delete(@PathVariable String alunoId) {
+		alunoRepository.delete(Long.valueOf(alunoId));
+		
+		return "redirect: /aluno/todos";
+	}
+
+	@RequestMapping(value = "/", method = {RequestMethod.POST, RequestMethod.PUT})
+	public String save(@RequestBody Aluno aluno) {
+		if (aluno == null) {
+			// TODO: Falta msg de erro!
+			return "redirect: /aluno/todos";
+		}
+		
+		aluno = alunoRepository.save(aluno);
+		return "redirect: /aluno/todos";
+	}
 }
