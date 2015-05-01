ecee88967a119b045cf8e288c4573a3ffbd8512f
David Robert <davidrobert@gmail.com>
2015-05-01 05:57:57 -0300

    Pequena correcao

--- src/main/java/br/com/while42/treinofitness/controller/web/WebMvcConfiguration.java
+++ src/main/java/br/com/while42/treinofitness/controller/web/WebMvcConfiguration.java
@@ -9,8 +9,8 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("homeNotSignedIn");
-        registry.addViewController("/").setViewName("homeNotSignedIn");
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
         registry.addViewController("/login").setViewName("login");
     }
 }
