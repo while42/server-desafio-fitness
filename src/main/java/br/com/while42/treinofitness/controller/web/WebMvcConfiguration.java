230aa1d9fb2586b6788c55f2327222b74d82bbcf
David Robert <davidrobert@gmail.com>
2015-05-01 15:37:08 -0300

    Removendo configuracao automativa para /login

--- src/main/java/br/com/while42/treinofitness/controller/web/WebMvcConfiguration.java
+++ src/main/java/br/com/while42/treinofitness/controller/web/WebMvcConfiguration.java
@@ -1,6 +1,7 @@
 package br.com.while42.treinofitness.controller.web;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -11,6 +12,7 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/login").setViewName("login");
+        
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
     }
 }
