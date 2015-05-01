5b339bd20cf7364728096367c43c43f77bfc4efc
David Robert <davidrobert@gmail.com>
2015-05-01 04:47:09 -0300

    Base para utilizacao do Spring Security

--- src/main/java/br/com/while42/treinofitness/SecurityConfiguration.java
+++ src/main/java/br/com/while42/treinofitness/SecurityConfiguration.java
@@ -1,33 +1,47 @@
 package br.com.while42.treinofitness;
 
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.builders.WebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @Configuration
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebMvcSecurity
 public class SecurityConfiguration 
-// extends WebSecurityConfigurerAdapter 
+ extends WebSecurityConfigurerAdapter 
 {
 
-//	@Override
-//	protected void configure(AuthenticationManagerBuilder auth)
-//			throws Exception {
-//
-//	}
-//
-//	@Override
-//	public void configure(WebSecurity web) throws Exception {
-//		web.ignoring().antMatchers("/status*");
-//	}
-//	
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//
-//		http.httpBasic().and().authorizeRequests().antMatchers("/status").permitAll();
-//		//http.authorizeRequests().anyRequest().anonymous();
-//	}
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth)
+			throws Exception {
+
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http
+        .authorizeRequests()
+            .antMatchers("/", "/api", "/api/status").permitAll()
+            .anyRequest().authenticated()
+            .and()
+        .formLogin()
+            .loginPage("/login")
+            .permitAll()
+            .and()
+        .logout()
+            .permitAll()
+            .and()
+        .csrf()
+            .disable();;
+            
+        // TODO: Somente para desenvolvimento
+        http.authorizeRequests().anyRequest().anonymous();    
+	}
 }
