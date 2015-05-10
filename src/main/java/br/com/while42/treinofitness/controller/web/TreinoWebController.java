2a547ca2eb9845366ce15e14714f6bfa055feede
ericvinicius <eric.camargo@gmail.com>
2015-05-10 02:18:55 -0300

    Adicionando debug para adicionar treino

--- src/main/java/br/com/while42/treinofitness/controller/web/TreinoWebController.java
+++ src/main/java/br/com/while42/treinofitness/controller/web/TreinoWebController.java
@@ -1,8 +1,11 @@
 package br.com.while42.treinofitness.controller.web;
 
+import lombok.extern.log4j.Log4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,28 +15,43 @@ import br.com.while42.treinofitness.model.Treino;
 import br.com.while42.treinofitness.repository.AlunoRepository;
 import br.com.while42.treinofitness.repository.TreinoRepository;
 
+@Log4j
 @Controller
 @RequestMapping("/treino")
 public class TreinoWebController {
 
 	private @Autowired TreinoRepository treinoRepository;
-	private @Autowired AlunoRepository AlunoRepository;
-	
+	private @Autowired AlunoRepository alunoRepository;
+
 	@RequestMapping(value = "/todos", method = RequestMethod.GET)
-	public String all(Model model){
-		Iterable<Treino> academias = treinoRepository.findAll();
-		
-		model.addAttribute("treinos", academias);
-		
+	public String all(Model model) {
+		Iterable<Treino> treinos = treinoRepository.findAll();
+
+		model.addAttribute("treinos", treinos);
+
 		return "treino-lista";
-	} 
-	
+	}
+
 	@RequestMapping(value = "/form", method = RequestMethod.GET)
-	public String form(@RequestParam long alunoId, Model model){
-		Aluno aluno = AlunoRepository.findOne(alunoId);
-		
+	public String form(@RequestParam long alunoId, Model model) {
+		Aluno aluno = alunoRepository.findOne(alunoId);
+
 		model.addAttribute("aluno", aluno);
-		
+		model.addAttribute("treino", new Treino("", ""));
+		log.debug("foi para a pagina do form");
+
 		return "treino-form";
-	} 
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public String save(@ModelAttribute Treino treino, @RequestParam Long alunoId) {
+
+		Aluno aluno = alunoRepository.findOne(alunoId);
+		aluno.addTreino(treino);
+		log.debug("Aluno ja tem exercicio");
+		alunoRepository.save(aluno);
+		log.debug("Aluno salvo no repositorio");
+
+		return "redirect:/treino/todos";
+	}
 }
