4829132dfaa8da2f65c9660518201b137193ee37
ericvinicius <eric.camargo@gmail.com>
2015-05-16 23:39:53 -0300

    Adicionando tooltip ao treino

--- src/main/java/br/com/while42/treinofitness/controller/web/ExercicioTempoWebController.java
+++ src/main/java/br/com/while42/treinofitness/controller/web/ExercicioTempoWebController.java
@@ -1,7 +1,5 @@
 package br.com.while42.treinofitness.controller.web;
 
-import lombok.extern.log4j.Log4j;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
